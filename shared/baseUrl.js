fcac78902ccc0a9e8717d81a2faab4b3c8667d94
Aashish Prasad <52325555+aashishwastaken@users.noreply.github.com>
2020-10-18 02:37:54 +0530

    JSON server url added

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,2 +1,3 @@
-export const baseUrl="https://raw.githubusercontent.com/aashishwastaken/Banja-aRestaurantApp/main/json-server/db.json/";
+export const baseUrl="https://raw.githubusercontent.com/aashishwastaken/Banja-aRestaurantApp/main/public/";
+export const baseUrlData="https://my-json-server.typicode.com/aashishwastaken/Banja-aRestaurantApp/";
 export default baseUrl;
